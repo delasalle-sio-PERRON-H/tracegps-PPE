542059f2605c2919e142da0442f6713b3bf53e6e
Shobu <shobu13@hotmail.fr>
2018-10-09 17:26:59 +0200

    ajout test unitaires

--- modele/tests/OutilsTest.php
+++ modele/tests/OutilsTest.php
@@ -6,31 +6,78 @@
  * Time: 14:54
  */
 
+include_once ('../Outils.class.php');
 
 class OutilsTest extends PHPUnit_Framework_TestCase
 {
 
-    public function testConvertirEnDateFR()
+    public function testConvertirEnDateFr()
     {
-        self::assertTrue(true);
+        $uneDateUS = '2007-05-16';
+        $uneDateFR = Outils::convertirEnDateFR($uneDateUS);
+        self::assertEquals($uneDateFR, '16/05/2007');
+    }
+
+    public function testConvertirEnDateUS()
+    {
+        $uneDateFR = '16/05/2007';
+        $uneDateUS = Outils::convertirEnDateUS($uneDateFR);
+        self::assertEquals($uneDateUS, '2007-05-16');
+    }
+
+    public function testCorrigerDate()
+    {
+        $uneDateAvant = '16-05-2007';
+        $uneDateApres = Outils::corrigerDate($uneDateAvant);
+        self::assertEquals($uneDateApres, '16/05/2007');
+    }
+
+    public function testCorrigerPrenom()
+    {
+        $unPrenomAvant = 'charles';
+        $unPrenomApres = Outils::corrigerPrenom($unPrenomAvant);
+        self::assertEquals($unPrenomApres, 'Charles');
+
+        $unPrenomAvant = 'charles-edouard';
+        $unPrenomApres = Outils::corrigerPrenom($unPrenomAvant);
+        self::assertEquals($unPrenomApres, 'Charles-Edouard');
+    }
+
+    public function testCorrigerTelephone()
+    {
+        $unNumeroAvant = '1122334455';
+        $unNumeroApres = Outils::corrigerTelephone($unNumeroAvant);
+        self::assertEquals($unNumeroApres, '11.22.33.44.55');
+
+        $unNumeroAvant = '11 22 33 44 55';
+        $unNumeroApres = Outils::corrigerTelephone($unNumeroAvant);
+        self::assertEquals($unNumeroApres, '11.22.33.44.55');
     }
 
     public function testCorrigerVille()
     {
+        $uneVilleAvant = 'rennes';
+        $uneVilleApres = Outils::corrigerVille($uneVilleAvant);
+        self::assertEquals($uneVilleApres, 'RENNES');
 
+        $uneVilleAvant = 'saint malo';
+        $uneVilleApres = Outils::corrigerVille($uneVilleAvant);
+        self::assertEquals($uneVilleApres, 'St MALO');
+
+        $uneVilleAvant = 'saint-malo';
+        $uneVilleApres = Outils::corrigerVille($uneVilleAvant);
+        self::assertEquals($uneVilleApres, 'St MALO');
     }
 
     public function testEstUneDateValide()
     {
-
+        $unCP = '35000';
+        assertTrue(Outils::estUnCodePostalValide($unCP));
+        $unCP = '3500';
+        self::assertFalse(Outils::estUnCodePostalValide($unCP));
     }
 
     public function testEstUneAdrMailValide()
-    {
-
-    }
-
-    public function testCorrigerTelephone()
     {
 
     }
@@ -45,23 +92,9 @@ class OutilsTest extends PHPUnit_Framework_TestCase
 
     }
 
-    public function testCorrigerDate()
-    {
-
-    }
-
-    public function testCorrigerPrenom()
-    {
-
-    }
-
     public function testEstUnNumTelValide()
     {
 
     }
 
-    public function testConvertirEnDateUS()
-    {
-
-    }
 }
