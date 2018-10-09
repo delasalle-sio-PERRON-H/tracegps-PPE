cf63b42b70b6ca684fc54bdac08bdd63c2b12df6
Unknown <shobu13@hotmail.fr>
2018-10-09 15:15:34 +0200

    ajout framework test unitaire

---
+++ modele/tests/OutilsTest.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lelu.a
+ * Date: 09/10/2018
+ * Time: 14:54
+ */
+
+
+class OutilsTest extends PHPUnit_Framework_TestCase
+{
+
+
+    public function testConvertirEnDateFR()
+    {
+
+    }
+
+    public function testCorrigerVille()
+    {
+
+    }
+
+    public function testEstUneDateValide()
+    {
+
+    }
+
+    public function testEstUneAdrMailValide()
+    {
+
+    }
+
+    public function testCorrigerTelephone()
+    {
+
+    }
+
+    public function testEnvoyerMail()
+    {
+
+    }
+
+    public function testEstUnCodePostalValide()
+    {
+
+    }
+
+    public function testCorrigerDate()
+    {
+
+    }
+
+    public function testCorrigerPrenom()
+    {
+
+    }
+
+    public function testEstUnNumTelValide()
+    {
+
+    }
+
+    public function testConvertirEnDateUS()
+    {
+
+    }
+}
